778c32adf3b4218b57a97d4d0fcecd0e03299766
Vaibhav Prasad <vaibhavp220604@gmail.com>
2024-07-17 16:17:54 +0530

    Added Searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import NavBar from './Components/NavBar.js'
 import News from './Components/News.js'
-import History from './Components/History.js'
 import { Route,Routes } from 'react-router-dom';
 import F404 from './Components/404.js';
 import { useState } from 'react';
-// import Auth from './Components/Auth.js';
 
 export function App() {
   const [category,setCategory] = useState("general");
@@ -14,8 +12,6 @@ export function App() {
       <NavBar setCategory={setCategory}/>
       <Routes>
       <Route path="/" element={<News category={category}/>} />
-      {/* <Route path="/" element={<Auth/>} /> */}
-      <Route path="/blogs" element={<History />} />
       <Route path="*" element={<F404/>} />
       </Routes>
     </>
